d24b83bd2de7a7504f939d50a635b488310c86d1
jakeemisrael <jakeemisrael@jakeems-air.lan>
2023-09-05 21:35:53 -0400

    This is my is first commit.

---
+++ LSP_Fall_2023/src/org/howard/edu/lsp/assignment1/Hello_World.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Jakeem_Israel
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
